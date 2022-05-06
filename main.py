891307bd16a5cd3ce6cf819d5b30fc4e9b4fa14f
RheniumGames <re2210games@gmail.com>
2022-05-06 22:50:32 +1200

    Scoring System

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 # A revision tool that can use user created questions and answers and present
 # them in a quiz format.
 
-import tkinter as tk
-import random
 import json
 import os
-
+import random
+import time
+import tkinter as tk
 
 FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 questions = []
 choices = []
 answers = []
 current_choices = []
+score = 0
 
 
 # Take data from questions.json
@@ -28,8 +29,6 @@ for item in data["QuestionList"]:
     if 'answer' in item:
         answers.append(item['answer'])
 
-print(choices)
-
 
 class InvalidList(Exception):
     pass
@@ -43,44 +42,67 @@ def import_list():
 
 
 def main():
-    score = 0
+    row_number = 1
     window = tk.Tk()
     window.title("Revision Tool")
     window.geometry("1280x720")
     window.columnconfigure([0, 1], weight=1, uniform="group1")
-    window.rowconfigure([0, 1, 2], weight=1)
+    window.rowconfigure([1, 2, 3], weight=1)
+    window.resizable(False, False)
+
     
 
     def new_question():
-        # Clear the possible current_choices
-        current_choices = []
-        question.configure(text=questions[0])
-        for i in range(0, 4):
-            print(choices[i])
-            current_choices.append(choices[i])
-        # Clear the used values from the default lists
-        choices[:4] = []
-        questions[:1] = []
-        print(choices)
-        print(questions)
-        random.shuffle(current_choices)
-        answer1.configure(text=current_choices[0])
-        answer2.configure(text=current_choices[1])
-        answer3.configure(text=current_choices[2])
-        answer4.configure(text=current_choices[3])
-        print('test')
+        try:
+            # Clear the possible current_choices
+            current_choices = []
+            question.configure(text=questions[0])
+            for i in range(0, 4):
+                current_choices.append(choices[i])
+            # Clear the used values from the default lists
+            choices[:4] = []
+            questions[:1] = []
+            random.shuffle(current_choices)
+            answer1.configure(text=current_choices[0])
+            answer2.configure(text=current_choices[1])
+            answer3.configure(text=current_choices[2])
+            answer4.configure(text=current_choices[3])
+        except IndexError:
+            question.configure(text="The quiz is over!")
+            answer1.destroy()
+            answer2.destroy()
+            answer3.destroy()
+            answer4.destroy()
+            next_button.destroy()
+            window.rowconfigure([2, 3], weight=0)
 
     
-    def check_answer(answer, score):
+    def check_answer(answer):
+        global score
         try:
             if answer == answers[0]:
                 score += 1
-                print('Correct!')
-                return score
-            else:
-                print('Incorrect!')
+                score_display.configure(text=f"Score: {str(score)}")
+            answers[:1] = []
+            new_question()
+            return score
         except IndexError:
             pass
+    
+
+    score_display = tk.Label(
+        window,
+        text=f"Score: {str(score)}",
+        font=("Helvetica", 20),
+        height=1,
+        )
+    score_display.grid(
+        row=row_number-1,
+        column=0,
+        columnspan=2,
+        sticky="s",
+        pady=5
+        )
 
 
     # Use a grid to present possible answers in a 2x2
@@ -90,7 +112,7 @@ def main():
         font=("Helvetica", 20)
         )
     question.grid(
-        row=0,
+        row=row_number,
         column=0,
         columnspan=2,
         sticky="nsew"
@@ -100,10 +122,10 @@ def main():
         window,
         text="Answer 1",
         font=("Helvetica", 16),
-        command=lambda: check_answer(answer1.cget("text"), score)
+        command=lambda: check_answer(answer1.cget("text"))
         )
     answer1.grid(
-        row=1,
+        row=row_number+1,
         column=0,
         sticky="nsew"
         )
@@ -112,10 +134,10 @@ def main():
         window,
         text="Answer 2",
         font=("Helvetica", 16),
-        command=lambda: check_answer(answer2.cget("text"), score)
+        command=lambda: check_answer(answer2.cget("text"))
         )
     answer2.grid(
-        row=1,
+        row=row_number+1,
         column=1,
         sticky="nsew"
         )
@@ -124,10 +146,10 @@ def main():
         window,
         text="Answer 3",
         font=("Helvetica", 16),
-        command=lambda: check_answer(answer3.cget("text"), score)
+        command=lambda: check_answer(answer3.cget("text"))
         )
     answer3.grid(
-        row=2,
+        row=row_number+2,
         column=0,
         sticky="nsew"
         )
@@ -136,10 +158,10 @@ def main():
         window,
         text="Answer 4",
         font=("Helvetica", 16),
-        command=lambda: check_answer(answer4.cget("text"), score)
+        command=lambda: check_answer(answer4.cget("text"))
         )
     answer4.grid(
-        row=2,
+        row=row_number+2,
         column=1,
         sticky="nsew"
         )
@@ -153,17 +175,12 @@ def main():
         command=lambda: new_question()
         )
     # Get the text in the next_button variable and print it
-    print(next_button['text'])
     next_button.grid(
-        row=3,
+        row=row_number+3,
         column=0,
         columnspan=2,
         sticky="nsew"
         )
-    print(questions)
-    print(choices)
-    print(answers)
-    print(type(data["QuestionList"][0]["options"]))
     window.mainloop()
 
 
